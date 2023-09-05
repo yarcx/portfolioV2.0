e13f1c042494f57cdf32c587caa8f0d963c01f4a
yarcx <yakubuh56@gmail.com>
2023-09-05 09:02:51 +0100

    Added the Side nav on mobile

--- src/components/PageInfoHeader.tsx
+++ src/components/PageInfoHeader.tsx
@@ -1,6 +1,22 @@
-import { HStack, IconButton, Text, useColorMode } from "@chakra-ui/react";
+import {
+  HStack,
+  IconButton,
+  Text,
+  useColorMode,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  Input,
+  useDisclosure,
+  Drawer,
+  Button,
+} from "@chakra-ui/react";
 import { GiNightSky } from "react-icons/gi";
 import { FaSun } from "react-icons/fa";
+import { AiOutlineMenu } from "react-icons/ai";
 import { MdOutlineKeyboardBackspace } from "react-icons/md";
 import { DARK_MODE, HOME, LIGHT_MODE, PageLink } from "../utils/constants";
 import { useLocation, useNavigate } from "react-router-dom";
@@ -11,38 +27,71 @@ const PageInfoHeader = () => {
   const navigate = useNavigate();
   const currentPage = PageLink.find((page) => page.route === pathname)?.link;
 
+  const { isOpen, onClose, onOpen } = useDisclosure();
+
   return (
-    <HStack as='header' justifyContent='space-between' p={3}>
-      <HStack as='div'>
-        {currentPage !== HOME && (
+    <>
+      <HStack as='header' justifyContent='space-between' p={3}>
+        <HStack as='div'>
+          {currentPage !== HOME && (
+            <IconButton
+              aria-label='Go Back'
+              background='none'
+              _hover={{ opacity: ".9" }}
+              isRound
+              display={["none", "", "", "", "block"]}
+              onClick={() => navigate(-1)}
+              icon={<MdOutlineKeyboardBackspace />}
+            />
+          )}
           <IconButton
             aria-label='Go Back'
             background='none'
             _hover={{ opacity: ".9" }}
             isRound
-            onClick={() => navigate(-1)}
-            icon={<MdOutlineKeyboardBackspace />}
+            display={["block", "", "", "", "none"]}
+            onClick={() => onOpen()}
+            minW={0}
+            icon={<AiOutlineMenu />}
+          />
+          <Text>{currentPage || HOME}</Text>
+        </HStack>
+
+        {colorMode === LIGHT_MODE ? (
+          <IconButton
+            aria-label='Set Light Mode'
+            onClick={() => setColorMode(DARK_MODE)}
+            icon={<GiNightSky size={24} />}
+            isRound
+          />
+        ) : (
+          <IconButton
+            aria-label='Set Dark Mode'
+            isRound
+            onClick={() => setColorMode(LIGHT_MODE)}
+            icon={<FaSun size={24} />}
           />
         )}
-        <Text>{currentPage || HOME}</Text>
       </HStack>
+      <Drawer isOpen={isOpen} placement='left' onClose={onClose}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>Create your account</DrawerHeader>
 
-      {colorMode === LIGHT_MODE ? (
-        <IconButton
-          aria-label='Set Light Mode'
-          onClick={() => setColorMode(DARK_MODE)}
-          icon={<GiNightSky size={24} />}
-          isRound
-        />
-      ) : (
-        <IconButton
-          aria-label='Set Dark Mode'
-          isRound
-          onClick={() => setColorMode(LIGHT_MODE)}
-          icon={<FaSun size={24} />}
-        />
-      )}
-    </HStack>
+          <DrawerBody>
+            <Input placeholder='Type here...' />
+          </DrawerBody>
+
+          <DrawerFooter>
+            <Button variant='outline' mr={3} onClick={onClose}>
+              Cancel
+            </Button>
+            <Button colorScheme='blue'>Save</Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </>
   );
 };
 
