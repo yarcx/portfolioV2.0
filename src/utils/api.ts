18dbbbd197c62474731caf49da60677d9a1bec7c
yarcx <yakubuh56@gmail.com>
2023-10-18 23:34:54 +0100

    fixed env import error

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -5,8 +5,8 @@ const supabase = createClient(supabaseUrl, supabaseKey);
 
 export const getURL = () => {
   let url =
-    process?.env?.VITE_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
-    process?.env?.VITE_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+    import.meta.env.VITE_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+    import.meta.env.VITE_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
     "http://localhost:3000/";
   // Make sure to include `https://` when not localhost.
   url = url.includes("http") ? url : `https://${url}`;
